23b5890084da4437f08d30810aadde0124995b61
Cian Jinks <cjinks99@gmail.com>
2022-06-10 10:39:27 +0100

    Running specific function defined in .py file
    There are two ways we could have approached doing this:
    1. Import the .py file as a module. The problem is this requires
    the .py file to be located on the PYTHONPATH environment variable.
    2. Run the python file to make it define the functions. Then the
    functions are available in the local namespace.

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,8 @@
-import vector
-from time import time, ctime
-print("Today is", ctime(time()))
+print("Test")
+
+def multiply(a,b):
+    print("Will compute", a, "times", b)
+    c = 0
+    for i in range(0, a):
+        c = c + b
+    return c
--- src/main.cpp
+++ src/main.cpp
@@ -33,27 +33,107 @@ static struct _inittab custom_modules[] = {
     {NULL, NULL}
 };
 
-int main(int argc, char *argv[])
+static void test_vec2()
 {
     vec2 a(3, 3);
     vec2 b(2, 2);
     vec2 r = mul_v2(a, b);
     r.print();
+}
 
-    FILE *fp = fopen("./scripts/test.py", "r");
-    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
-    if (program == NULL) {
-        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-        exit(1);
+int main(int argc, char *argv[])
+{
+    test_vec2();
+
+    PyObject *modules = NULL, *builtins = NULL, *mod_main = NULL;
+    PyObject *py_result = NULL, *py_dict = NULL;
+    PyObject *pName = NULL, *pFunc = NULL;
+    PyObject *pArgs = NULL, *pValue = NULL;
+
+    long array[2] = {2, 3};
+
+    const char* filename = "test.py";
+    const char* filepath = "./scripts/test.py";
+    FILE *fp = fopen(filepath, "r");
+    if(fp)
+    {
+        wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+        if (program == NULL) {
+            printf("Fatal error: cannot decode argv[0]\n");
+            return 1;
+        }
+        PyImport_ExtendInittab(custom_modules);
+        Py_SetProgramName(program);  /* optional but recommended */
+        Py_Initialize();
+        modules = PyImport_GetModuleDict();
+        builtins = PyEval_GetBuiltins();
+        mod_main = PyModule_New("__main__");
+        PyDict_SetItemString(modules, "__main__", mod_main);
+        Py_DECREF(mod_main); /* sys.modules owns now */
+        PyModule_AddStringConstant(mod_main, "__name__", "__main__");
+        PyModule_AddObject(mod_main, "__file__", PyUnicode_FromStringAndSize(filename, strlen(filename)));
+        PyModule_AddObject(mod_main, "__builtins__", builtins);
+        Py_INCREF(builtins); /* AddObject steals a reference */
+        py_dict = PyModule_GetDict(mod_main);
+        py_result = PyRun_File(fp, filepath, Py_file_input, py_dict, py_dict);
+
+        /* Test running function from module. */
+        if (mod_main != NULL) {
+            pFunc = PyObject_GetAttrString(mod_main, "multiply");
+            /* pFunc is a new reference */
+
+            if (pFunc && PyCallable_Check(pFunc)) {
+                pArgs = PyTuple_New(2);
+                for (int i = 0; i < 2; ++i) {
+                    pValue = PyLong_FromLong(array[i]);
+                    if (!pValue) {
+                        Py_DECREF(pArgs);
+                        Py_DECREF(mod_main);
+                        printf("Cannot convert argument\n");
+                        return 1;
+                    }
+                    /* pValue reference stolen here: */
+                    PyTuple_SetItem(pArgs, i, pValue);
+                }
+                pValue = PyObject_CallObject(pFunc, pArgs);
+                Py_DECREF(pArgs);
+                if (pValue != NULL) {
+                    printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                    Py_DECREF(pValue);
+                }
+                else {
+                    Py_DECREF(pFunc);
+                    Py_DECREF(mod_main);
+                    PyErr_Print();
+                    printf("Call failed\n");
+                    return 1;
+                }
+            }
+            else {
+                if (PyErr_Occurred())
+                    PyErr_Print();
+                printf("Cannot find function multiply\n");
+            }
+            Py_XDECREF(pFunc);
+            Py_DECREF(mod_main);
+        }
+        else {
+            PyErr_Print();
+            printf("Failed to load module\n");
+            return 1;
+        }
+
+        if (Py_FinalizeEx() < 0) {
+            printf("Interpreter failed to exit correctly :(\n");
+            return 1;
+        }
+        PyMem_RawFree(program);
+        fclose(fp);
     }
-    PyImport_ExtendInittab(custom_modules);
-    Py_SetProgramName(program);  /* optional but recommended */
-    Py_Initialize();
-    PyRun_SimpleFile(fp, "test.py");
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
+    else
+    {
+        fprintf(stderr, "Failed to load filepath: %s\n", filepath);
+        return 1;
     }
-    PyMem_RawFree(program);
-    fclose(fp);
     return 0;
 }
